e3b43db5e6a135bcd8e5599398fa970350fac6cf
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2018-05-23 20:05:09 +0200

    Add tests for uuid v4 module

--- tests/helpers/uuidv4.test.js
+++ tests/helpers/uuidv4.test.js
@@ -8,4 +8,14 @@ describe('uuidv4', function () {
   test('should export a function', () => {
     expect(typeof underTest).toBe('function');
   });
+
+  test('should generate valid uuid v4', () => {
+    const uuidv4_regex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
+    expect(uuidv4_regex.test(underTest())).toBeTruthy();
+  });
+
+  test('should generate different values when it is executed multiple times', () => {
+    expect(underTest()).not.toBe(underTest());
+  });
 });
