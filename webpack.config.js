0ada55d9516005494f23e89b422954cbb79ff5a3
Gojko Adzic <gojko@gojko.com>
2018-05-25 10:05:16 +0200

    added SRI output for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
-const path = require('path')
+const path = require('path'),
+  fs = require('fs'),
+  SriPlugin = require('webpack-subresource-integrity'),
+  PrintIntegrityPlugin = function () {
+    'use strict';
+    this.apply = function (compiler) {
+      compiler.hooks.done.tap('integrity', (stats) => {
+        const mainAssetName = stats.toJson().assetsByChunkName.main,
+          integrity = stats.compilation.assets[mainAssetName].integrity;
+        console.log('integrity', integrity);
+        fs.writeFileSync(path.resolve(__dirname, 'dist', 'sri-integrity.txt'), integrity, 'utf8');
+      });
+    };
+  };
 
 module.exports = {
   mode: 'production',
   entry: './src/desole.js',
   output: {
+    crossOriginLoading: 'anonymous',
     library: 'Desole',
     libraryTarget: 'window',
-    filename: 'desole.js',
+    filename: 'client-min.js',
     path: path.resolve(__dirname, 'dist')
-  }
-}
+  },
+  plugins: [
+    new SriPlugin({
+      hashFuncNames: ['sha256', 'sha384'],
+      enabled: true
+    }),
+    new PrintIntegrityPlugin()
+  ]
+};
